ed75a9f13e3963790635b224e9b2f1fd8095c177
Ehuntwork <sir.mle.learnsalot@gmail.com>
2020-12-17 10:13:48 -0800

    Homeless_info ready to post

--- src/components/pages/IntakePacketContent/ByGuests/HomelessHistory.js
+++ src/components/pages/IntakePacketContent/ByGuests/HomelessHistory.js
@@ -8,8 +8,9 @@ import {
   Checkbox,
   Card,
 } from 'antd';
-const HomelessHistory = ({ navigation, tempFormStyle }) => {
+const HomelessHistory = ({ navigation, formData, setForm, tempFormStyle }) => {
   const { previous, next } = navigation;
+  const { familyInfo } = formData;
   return (
     <div style={tempFormStyle}>
       <Card title="History" bordered={false}>
@@ -23,39 +24,55 @@ const HomelessHistory = ({ navigation, tempFormStyle }) => {
         </Form.Item>
         <Form layout="vertical">
           <Form.Item label="Last permanent address (last address you lived where you did not consider yourself to be homeless">
-            <Input />
+            <Input
+              name={'familyInfo.last_permanent_address'}
+              value={familyInfo.last_permanent_address}
+              onChange={setForm}
+            />
           </Form.Item>
-          <Form.Item label="Where did you stay last night? How long were you at this location?">
-            <Input.Group style={{ display: 'flex' }}>
-              <Form.Item label="Location">
-                <Input />
-              </Form.Item>
-              <Form.Item label="Days">
-                <InputNumber />
-              </Form.Item>
-              <Form.Item label="Months">
-                <InputNumber />
-              </Form.Item>
-              <Form.Item label="Years">
-                <InputNumber />
-              </Form.Item>
-            </Input.Group>
+          <Form.Item label="Where did you stay last night?">
+            <Input
+              name={'familyInfo.homeless_info.prior_location'}
+              value={familyInfo.homeless_info.prior_location}
+              onChange={setForm}
+            />
+          </Form.Item>
+          <Form.Item label="How long were you at this location?">
+            <Form.Item label="Days">
+              <Input
+                name={'familyInfo.homeless_info.length_at_prior_location'}
+                value={familyInfo.homeless_info.length_at_prior_location}
+                onChange={setForm}
+              />
+            </Form.Item>
           </Form.Item>
           <Form.Item label="Aprroximately when did you become homeless?">
-            <DatePicker format="DD/MM/YYYY" />
+            <Input
+              name={'familyInfo.homeless_info.homeless_start_date'}
+              value={familyInfo.homeless_info.homeless_start_date}
+              onChange={setForm}
+            />
           </Form.Item>
           <Form.Item label="How many times in the last 3 years have you been homeless?">
-            <InputNumber />
+            <Input
+              name={'familyInfo.homeless_info.num_times_homeless'}
+              value={familyInfo.homeless_info.num_times_homeless}
+              onChange={setForm}
+            />
           </Form.Item>
           <Form.Item label="How many total months in those 3 years have you been homeless?">
-            <InputNumber />
-          </Form.Item>
-          <Form.Item label="Have you used our shelter before?">
-            <Checkbox>Y</Checkbox>
-            <Checkbox>N</Checkbox>
+            <Input
+              name={'familyInfo.homeless_info.total_len_homeless'}
+              value={familyInfo.homeless_info.total_len_homeless}
+              onChange={setForm}
+            />
           </Form.Item>
           <Form.Item label=" If yes when?">
-            <Input />
+            <Input
+              name={'familyInfo.homeless_info.length_at_current_location'}
+              value={familyInfo.homeless_info.length_at_current_location}
+              onChange={setForm}
+            />
           </Form.Item>
         </Form>
       </Card>
