52fbb62cec187935a5f770d8e45c17a70e8d8cd0
Abdi Mohamud <abdinajibmohamud@gmail.com>
2020-12-31 15:04:13 -0800

    got functionality on the first page to display progress in console, debugging the onclick next button

--- src/components/pages/IntakePacketContent/ByGuests/ContactInfo.js
+++ src/components/pages/IntakePacketContent/ByGuests/ContactInfo.js
@@ -3,8 +3,44 @@ import { Form, Input, Space, Button, Card, Progress } from 'antd';
 
 import Checkbox from 'antd/lib/checkbox/Checkbox';
 const ContactInfo = ({ navigation, formData, setForm, tempFormStyle }) => {
+  Object.filter = (obj, predicate) =>
+    Object.assign(
+      ...Object.keys(obj)
+        .filter(key => predicate(obj[key]))
+        .map(key => ({ [key]: obj[key] }))
+    );
+  function ObjectLength(object) {
+    var length = 0;
+    for (var key in object) {
+      if (object.hasOwnProperty(key)) {
+        ++length;
+      }
+    }
+    return length;
+  }
+
   const { next, previous } = navigation;
   const { familyInfo } = formData;
+  const info = [familyInfo];
+  console.log(info);
+  const onProgress = () => {
+    const total =
+      ObjectLength(familyInfo.phone_one) +
+      ObjectLength(familyInfo.phone_two) +
+      ObjectLength(familyInfo.emergencyContact);
+    const phone1 =
+      Object.filter(familyInfo.phone_one, item => item != null) || {};
+    const phone2 =
+      Object.filter(familyInfo.phone_two, item => item != null) || {};
+    const emerg =
+      Object.filter(familyInfo.emergencyContact, item => item != null) || {};
+    const sum =
+      Object.keys(phone1).length +
+      Object.keys(phone2).length +
+      Object.keys(emerg).length;
+    console.log(`${sum}/${total}`);
+    return next;
+  };
 
   return (
     <div style={tempFormStyle}>
@@ -15,7 +51,7 @@ const ContactInfo = ({ navigation, formData, setForm, tempFormStyle }) => {
             <Button type="primary" htmlType="button" onClick={previous}>
               Previous
             </Button>
-            <Button type="primary" htmlType="button" onClick={next}>
+            <Button type="primary" htmlType="button" onClick={onProgress}>
               Next
             </Button>
           </Form.Item>
