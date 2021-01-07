e94df786e7199d2d831993b428289c4ba5fc8f06
Leslie Rodriguez <rleslie1015@gmail.com>
2021-01-07 15:30:07 -0600

    adds documentation

--- src/state/types/index.js
+++ src/state/types/index.js
@@ -1,3 +1,9 @@
+// we started to transition into declaring user types in this file
+// although you can probabably tell some of these we didn't get enough time to start using
+// you will find in some files throughout the app we did data fetching in the useEffect hook
+// something the next team can focus on is centralizing all state and data fetching to come from redux
+// declare all types here and import them in the reducer/action files
+
 // user types
 export const LOG_IN = 'LOG_IN';
 export const SET_LOADING = 'SET_LOADING';
@@ -8,7 +14,7 @@ export const GET_FAMILY_FETCHING = 'GET_FAMILY_FETCHING';
 export const GET_FAMILY_SUCCESS = 'GET_FAMILY_SUCCESS';
 export const GET_FAMILY_FAILURE = 'GET_FAMILY_FAILURE';
 
-// members types
+// members types (not currently being used)
 export const GET_MEMBERS_FETCHING = 'GET_MEMBERS_FETCHING';
 export const GET_MEMBERS_SUCCESS = 'GET_MEMBERS_SUCCESS';
 export const GET_MEMBERS_FAILURE = 'GET_MEMBERS_FAILURE';
