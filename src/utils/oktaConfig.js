25b83d84bcde446a73da15a78ebe24a111c3357a
Evan Barton <notrabe1011@gmail.com>
2021-02-11 17:15:00 -0500

    add facebook to signin widget

--- src/utils/oktaConfig.js
+++ src/utils/oktaConfig.js
@@ -4,8 +4,6 @@ const config = {
   clientId: process.env.REACT_APP_CLIENT_ID,
   pkce: true,
   scopes: ['openid', 'email', 'profile'],
-  idps: { type: 'Facebook', id: '446964073111312' },
-  idpDisplay: 'SECONDARY',
 };
 
 export { config };
