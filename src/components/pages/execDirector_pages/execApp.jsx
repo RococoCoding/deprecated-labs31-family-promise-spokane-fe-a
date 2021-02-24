3960eee2d1e3dbfdd59873de315f8ab11af375b7
unknown <fullstack.melissa@gmail.com>
2021-02-24 15:35:41 -0800

    revised exec dash

---
+++ src/components/pages/execDirector_pages/execApp.jsx
@@ -0,0 +1,25 @@
+/*import React from 'react';
+import { useState } from "react";
+import "./App.css";
+//import execDirectorAnalytics from "./execDirectorAnalytics.jsx";
+import Navbar from "./navBar";
+import Sidebar from "./sideBar";
+
+const execApp = () => {
+  const [sidebarOpen, setsidebarOpen] = useState(false);
+  const openSidebar = () => {
+    setsidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setsidebarOpen(false);
+  };
+  return (
+    <div className="container">
+      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+      <execDirectorAnalytics />
+      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+    </div>
+  );
+};
+
+export default execApp; */
