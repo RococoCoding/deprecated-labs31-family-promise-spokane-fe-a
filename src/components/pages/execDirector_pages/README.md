355ad3cb640e900ebe78b19807017bf2638bb9a0
unknown <fullstack.melissa@gmail.com>
2021-03-01 13:31:18 -0800

    sideBar updates

--- src/components/pages/execDirector_pages/README.md
+++ src/components/pages/execDirector_pages/README.md
@@ -5,6 +5,7 @@
 - We were not able to implement Ant Design, which must be researched further.
 - You must add react-plotly and remove react-vis to connect to the visualizations.
 - Login page needs to be directed to the dashboard.
+- FE deployment (https://a.familypromisesofspokane.dev/login)
 
 
 
--- src/components/pages/execDirector_pages/sideBar.jsx
+++ src/components/pages/execDirector_pages/sideBar.jsx
@@ -16,55 +16,47 @@ const Sidebar = ({ sidebarOpen, closeSidebar }) => {
       <div className="sidebar__menu">
         <div className="sidebar__link active_menu_link">
           <i className="fa fa-home"></i>
-          <a href="https://github.com/Lambda-School-Labs/family-promise-spokane-fe-a">
-            My Dashboard
-          </a>
+          <a href="https://a.familypromisesofspokane.dev/login">My Dashboard</a>
         </div>
         <h2>Operations</h2>
         <div className="sidebar__link">
           <i className="fa fa-user-secret" aria-hidden="true"></i>
-          <a href="https://github.com/Lambda-School-Labs/family-promise-spokane-fe-a">
+          <a href="https://a.familypromisesofspokane.dev/login">
             Management Duties
           </a>
         </div>
         <div className="sidebar__link">
           <i className="fas fa-clipboard"></i>
-          <a href="https://github.com/Lambda-School-Labs/family-promise-spokane-fe-a">
+          <a href="https://a.familypromisesofspokane.dev/login">
             Case Management
           </a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-building-o"></i>
-          <a href="https://github.com/Lambda-School-Labs/family-promise-spokane-fe-a">
+          <a href="https://a.familypromisesofspokane.dev/analytics">
             Analtyics
           </a>
         </div>
 
         <div className="sidebar__link">
           <i className="fa fa-user-o"></i>
-          <a href="https://github.com/Lambda-School-Labs/family-promise-spokane-fe-a">
+          <a href="https://a.familypromisesofspokane.dev/supervisor-checkin">
             Current Guests
           </a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-envelope"></i>
-          <a href="https://github.com/Lambda-School-Labs/family-promise-spokane-fe-a">
-            Messages
-          </a>
+          <a href="https://a.familypromisesofspokane.dev/login">Messages</a>
         </div>
 
         <div className="sidebar__link">
           <i className="fa fa-wrench"></i>
-          <a href="https://github.com/Lambda-School-Labs/family-promise-spokane-fe-a">
-            Resources
-          </a>
+          <a href="https://a.familypromisesofspokane.dev/login">Resources</a>
         </div>
 
         <div className="sidebar__logout">
           <i className="fa fa-power-off"></i>
-          <a href="https://github.com/Lambda-School-Labs/family-promise-spokane-fe-a">
-            Log out
-          </a>
+          <a href="https://a.familypromisesofspokane.dev/login">Log out</a>
         </div>
       </div>
     </div>
